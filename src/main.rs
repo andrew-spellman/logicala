d8ea93d7110684c813c744b177b265ef00098b7b
Andrew Spellman <andrew-spellman@protonmail.com>
2023-11-15 22:36:22 -0600

    thinking about it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+std::collections::HashMap;
+
+enum Literal {
+    B(bool)
+    Z(i32)
+}
+
+type VarName = String;
+
+enum Premise {
+    Equal(VarName, Literal),
+    NotEqual(VarName, Literal),
+    Implies(VarName, VarName),
+}
+
+struct Line {
+    lineNumber: i32,
+    claim: VarName,
+    justification: String,
+    proven: bool,
+}
+
+impl Line {
+    fn prove(&self, premises: Vec<Premise>, justifying_lines: Vec<Line>) {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
